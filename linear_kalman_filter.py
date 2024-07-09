3e0062bb17e88dacee4f3a07ca91edb3575f540f
John Ryu Adams <radams.official1@gmail.com>
2024-07-09 23:28:29 +0000

    implemented linear multivariate kalman filter

--- linear_kalman_filter.py
+++ linear_kalman_filter.py
@@ -1,28 +1,106 @@
 import numpy as np
 
+# from filterpy.common import Q_discrete_white_noise
+
 from collections import namedtuple
 gaussian = namedtuple('Gaussian', ['mean', 'var'])
 
-class kalman_filter():
-    def __init__(self, dim_x, dim_z):
+class KalmanFilter():
+    def __init__(self, state_vars, output_vars, x, P, F, Q, u, B, z, R, H):
         # initialize
-        print("")
+        self.x = x  # STATE MEAN
+        self.P = P  # STATE COVARIANCE
+        self.F = F  # STATE TRANSITION FUNCTION (PROCESS FUNCTION IN MATRIX FORM)
+        self.Q = Q  # PROCESS COVARIANCE (NOISE)
+        
+        self.u = u  # CONTROL INPUT
+        self.B = B  # CONTROL FUNCTION
+        
+        self.z = z  # MEASUREMENT MEAN
+        self.R = R  # MEASUREMENT COVARIANCE
+        self.H = H  # MEASUREMENT TRANSITION FUNCTION
+        self.y = None  # RESIDUAL (INITIALIZED LATER)
+        self.K = None  # KALMAN GAIN (INITIALIZED LATER)
+        
 
         
-    def update(prior, measurement):
-        x, P = prior        # mean and variance of prior
-        z, R = measurement  # mean and variance of measurement
+    def all_variables(self):
+        print(f"x: {self.x}\nP: {self.P}\nF: {self.F}\nQ: {self.Q}\nu: {self.u}\nB: {self.B}\nH: {self.H}\nz: {self.z}\nR: {self.R}\ny: {self.y}\nK: {self.K}")
+
         
-        y = z - x        # residual
-        K = P / (P + R)  # Kalman gain
+    def update(self, z):
+        self.z = z
 
-        x = x + K*y      # posterior    
-        P = (1 - K) * P  # posterior variance
-        return gaussian(x, P)
+        self.y = self.z - self.H @ self.x
+        
+        Ht = self.H.T
+        S = self.H @ self.P @ Ht + self.R
+        self.K = self.P @ Ht @ np.linalg.inv(S)
+        
+        self.x = self.x + self.K @ self.y
+        
+        I = np.ones_like(self.K @ self.H)
+        # I = np.eye(self.P.shape[0])  # Identity matrix
+        self.P = (I - self.K @ self.H) @ self.P
+        
+        
+        return (self.x, self.P)
+        
 
-    def predict(posterior, movement):
-        x, P = posterior # mean and variance of posterior
-        dx, Q = movement # mean and variance of movement
-        x = x + dx
-        P = P + Q
-        return gaussian(x, P)
+    def predict(self):
+        self.x = self.F @ self.x + self.B @ self.u
+        
+        Ft = self.F.T
+        self.P = self.F @ self.P @ Ft + self.Q
+
+
+        return (self.x, self.P)
+
+
+
+
+# TESTING
+
+x = np.array([[10.0],
+              [4.5]])
+
+P = np.array([[500., 0.],
+              [0., 49.]])
+
+dt = 0.1
+F = np.array([[1, dt],
+              [0, 1]])
+
+Q = np.array([[0.588, 1.175],
+              [1.175, 2.35 ]])
+
+u=np.array([[0]])
+B = np.array([0.])
+
+H = np.array([[1., 0.]])
+R = np.array([[5.]])
+
+
+
+# MEASUREMENT
+z = np.array([[20]])  
+
+kf = KalmanFilter(state_vars=2, output_vars=1, x=x, P=P, F=F, Q=Q, u=u, B=B, z=z, R=R, H=H)
+
+
+# Predict step
+print("PREDICTION:")
+x, P = kf.predict()
+print('x =', x)
+print('P =', P)
+
+# Update step with a new measurement
+print("\nUPDATE:")
+x, P = kf.update(z)
+print('x =', x)
+
+# Print all variables
+# kf.all_variables()
+
+
+# print(k.all_variables())
