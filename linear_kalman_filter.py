2c878b841c57bcab36a211381efc5b8c6a8353f3
John Ryu Adams <radams.official1@gmail.com>
2024-07-12 14:39:50 -0700

    add/remove comments for clarity

--- linear_kalman_filter.py
+++ linear_kalman_filter.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-# from filterpy.common import Q_discrete_white_noise
-
-from collections import namedtuple
-gaussian = namedtuple('Gaussian', ['mean', 'var'])
-
 class KalmanFilter():
     def __init__(self, state_vars, output_vars, x, P, F, Q, u, B, z, R, H):
         # initialize
@@ -59,31 +54,46 @@ class KalmanFilter():
 
 
 
-# TESTING
+## TESTING
 
+# STATE MEAN
 x = np.array([[10.0],
               [4.5]])
 
+# STATE COVARIANCE
 P = np.array([[500., 0.],
               [0., 49.]])
 
+# STATE TRANSITION FUNCTION (PROCESS FUNCTION IN MATRIX FORM)
 dt = 0.1
 F = np.array([[1, dt],
               [0, 1]])
 
+# PROCESS COVARIANCE (NOISE)
 Q = np.array([[0.588, 1.175],
               [1.175, 2.35 ]])
 
+
+
+# CONTROL INPUT
 u=np.array([[0]])
+
+# CONTROL FUNCTION
 B = np.array([0.])
 
+
+
+# MEASUREMENT TRANSITION FUNCTION
 H = np.array([[1., 0.]])
+
+# MEASUREMENT COVARIANCE
 R = np.array([[5.]])
 
 
 
-# MEASUREMENT
+### MEASUREMENT
 z = np.array([[20]])  
+
 
 kf = KalmanFilter(state_vars=2, output_vars=1, x=x, P=P, F=F, Q=Q, u=u, B=B, z=z, R=R, H=H)
 
@@ -101,6 +111,3 @@ print('x =', x)
 
 # Print all variables
 # kf.all_variables()
-
-
-# print(k.all_variables())
