c0afb3bab4ffa4b1ff7905725e82fcb0891b98a9
John Ryu Adams <radams.official1@gmail.com>
2024-07-16 18:32:05 -0700

    update ekf in ekf2 file, test more on casadi_test, test on tracking_plane file

--- casadi_test.py
+++ casadi_test.py
@@ -1,60 +1,18 @@
 import numpy as np
 import casadi as ca
-from casadi import SX, MX, jacobian, sqrt, sin, cos, vertcat, substitute
+from casadi import SX, MX, jacobian, sqrt, sin, cos, vcat, hcat, substitute, Function
 import sympy as sp
 
-"""
 
-# Create symbolic variables
-x = ca.MX.sym('x')
-y = ca.MX.sym('y')
-
-# Create functions
-f1 = x**2 + y
-f2 = ca.sin(x) - ca.cos(y)
-f3 = x*y
-f4 = ca.exp(x/y)
-
-# Create a matrix of functions
-matrix_of_functions = ca.vertcat(
-    ca.horzcat(f1, f2),
-    ca.horzcat(f3, f4)
-)
-
-# Print the matrix
-print(matrix_of_functions)
-
-# Compute the jacobian
-jacobian = ca.jacobian(matrix_of_functions, x)
-print(jacobian)
+# SYM VARIABLES
+xs = SX.sym('x')
+ys = SX.sym('y')
+alphas = SX.sym('alpha')
+betas = SX.sym('beta')
+thetas = SX.sym('theta')
 
 
-# Create a CasADi function that takes x and y as inputs and returns the matrix of functions
-func = ca.Function('func', [x, y], [matrix_of_functions])
-
-# Define the values for x and y
-x_val = 1.0
-y_val = 2.0
-
-# Evaluate the matrix of functions with the specific values
-result = func(x_val, y_val)
-
-# Print the result
-print(result)
-
-"""
-
-xs = MX.sym('x')
-ys = MX.sym('y')
-thetas = MX.sym('theta')
-alphas = MX.sym('alpha')
-betas = MX.sym('beta')
-rs = MX.sym('r')
-
-pxs = MX.sym('p_x')
-pys = MX.sym('p_y')
-
-'''
+""" 
 # STATE MEAN VECTOR
 x = ca.vcat([xs, ys, thetas])
 
@@ -74,47 +32,65 @@ Ft = F.T
 
 print("Ft:\n", Ft)
 
-'''
-
-z = ca.vcat([sqrt((pxs-xs)**2 + (pys-ys)**2),
-            ca.arctan2(pys-ys, pxs-xs) - thetas])
-
-print("z:\n", z)
-
-##### WRITE FUNCTIONS TO CONVERT casADi TO SymPy TO MAKE EQUATIONS READABLE
-""" 
-def casadi_to_sympy(expr):
-    # Convert a CasADi expression to a SymPy expression.
-    if isinstance(expr, ca.MX):
-        # Handle CasADi symbols
-        if expr.is_symbolic():
-            return sp.symbols(str(expr))
-        # Handle CasADi constants
-        elif expr.is_constant():
-            return sp.Float(float(expr))
-        # Handle CasADi expressions
-        else:
-            # Convert CasADi expression to string and parse it with SymPy
-            return sp.sympify(str(expr))
-    else:
-        raise TypeError("Unsupported type")
-
-def casadi_matrix_to_sympy(matrix):
-    # Convert a CasADi matrix to a SymPy matrix.
-    rows, cols = matrix.shape
-    sympy_matrix = sp.zeros(rows, cols)
-    for i in range(rows):
-        for j in range(cols):
-            sympy_matrix[i, j] = casadi_to_sympy(matrix[i, j])
-    return sympy_matrix
-
-def pretty_print_casadi_matrix(matrix):
-    # Pretty print a CasADi matrix using SymPy.
-    sympy_matrix = casadi_matrix_to_sympy(matrix)
-    sp.pprint(sympy_matrix)
-
-
-print("PRINTING: ")
-pretty_print_casadi_matrix(F) 
-
 """
+ 
+
+ 
+###USING THE Function FUNCTION IN casADi
+
+
+# SYM VARIABLES
+xs = SX.sym('x')
+ys = SX.sym('y')
+alphas = SX.sym('alpha')
+betas = SX.sym('beta')
+
+
+x = SX([3, 2])
+x_sym = vcat([xs, ys])
+
+u = SX([2, 5])
+u_sym = vcat([alphas, betas])
+
+
+
+f = vcat([xs*alphas, xs+betas, xs*ys])
+
+
+fxu = Function('f', [x_sym, u_sym], [f])
+
+
+print("f result:", fxu(x,u))
+print('r0:',fxu(x,u)[0])
+print('q0:',fxu(x,u)[1])
+print('t0:',fxu(x,u)[2])
+
+
+
+
+
+""" 
+### INV testing
+import casadi as ca
+import numpy as np
+
+# Define a CasADi symbolic matrix
+A = ca.SX([[1, 2], [3, 4]])
+B = np.array([[1, 2], [3, 4]])
+
+# Compute the inverse
+A_inv = ca.inv(A)
+B_inv = np.linalg.inv(B)
+
+# Print the result
+print(A_inv)
+print(B_inv)
+
+
+
+
+### SX_eye testing
+print(ca.SX_eye(4))
+ """
+ 
+ 
---
+++ ekf2.py
@@ -0,0 +1,74 @@
+# import numpy as np
+from casadi import SX, MX, jacobian, sqrt, sin, cos, arctan2, vcat, hcat, Function, inv, SX_eye
+
+
+class ExtendedKalmanFilter():
+    def __init__(self, state_vars, output_vars, control_vars, x0, x_sym, u, u_sym, P, f, M, h, R):
+        self.state_vars = state_vars
+        self.output_vars = output_vars
+        self.control_vars = control_vars
+        
+        self.x = x0  # STATE MEAN 
+        self.x_sym = x_sym  # STATE MEAN (SYMBOLIC VARAIABLES)
+        self.P = P  # STATE COVARIANCE
+    
+        self.f = f  # STATE TRANSITION FUNCTION (SYMBOLIC VARIABLES)
+        self.M = M  # PROCESS COVARIANCE/NOISE (aka Q)
+        
+        self.u = u  # CONTROL INPUT
+        self.u_sym = u_sym  # CONTROL INPUT (SYMBOLIC VARIABLES)
+        
+        self.z = SX(state_vars, 1)  # MEASUREMENT MEAN
+        self.R = R # MEASUREMENT COVARIANCE
+        
+        self.h = h # MEASUREMENT TRANSITION FUNCTION (SYMBOLIC VARIABLES)
+        
+        
+        self.fxu = Function('fxu', [self.x_sym, self.u_sym], [self.f])  
+        
+        self.hx = Function('hx', [self.x_sym], [self.h])
+        
+        self.F = jacobian(self.f, self.x_sym)
+        self.Fxu = Function('Fxu', [self.x_sym, self.u_sym], [self.F])  
+        
+        self.H = jacobian(self.h, self.x_sym)
+        self.Hx = Function('Hx', [self.x_sym], [self.H])
+        
+        self.V = jacobian(self.f, self.u_sym)
+        self.Vxu = Function('Vxu', [self.x_sym, self.u_sym], [self.V]) 
+        
+        
+
+
+        
+    def update(self, z):
+        self.z = z  # MEASUREMENT MEAN (MUST BE MATRIX (MX))
+
+        self.y = self.z - self.hx(self.x)  # PLUG IN VALUES OF STATE MEAN x INTO FUNCTION hx    
+        
+        self.H = self.Hx(self.x)
+        Ht = self.H.T
+        S = self.H @ self.P @ Ht + self.R
+        self.K = self.P @ Ht @ inv(S)
+        
+        self.x = self.x + self.K @ self.y
+
+        I = SX_eye(self.P.shape[0])  # Identity matrix
+        self.P = (I - self.K @ self.H) @ self.P
+        
+        
+        return (self.x, self.P)
+        
+
+
+
+    def predict(self):
+        self.x = self.fxu(self.x)
+
+        self.F = self.Fxu(self.x, self.u)
+        Ft = self.F.T
+        self.V = self.Vxu(self.x, self.u)
+        Vt = self.V.T
+        self.P = self.F @ self.P @ Ft + self.V @ self.M @ Vt
+        
+        return (self.x, self.P)
